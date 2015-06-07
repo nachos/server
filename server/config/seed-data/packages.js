8dae7116be89252e20f47f82cb09b30b9c3954c5
Elad Bezalel <elad.bezalel@gmail.com>
2015-06-07 21:58:49 +0300

    Changed seed to the new repos

--- server/config/seed-data/packages.js
+++ server/config/seed-data/packages.js
@@ -5,17 +5,24 @@ var Package = require('../../api/package/package.model');
 module.exports = function () {
   var deferred = Q.defer();
   Package.find({}).remove(function () {
-    Package.create({
-      name: 'movie-list',
-      repo: 'git://github.com/nachos/movie-list.git',
-      type: 'dip'
-    }, function (err) {
-      if (err) {
-        deferred.reject(err);
-      } else {
-        deferred.resolve(_.toArray(arguments).slice(1));
-      }
-    });
+    Package.create(
+      {
+        name: 'movie-list',
+        repo: 'git://github.com/nachos/dip-movie-list.git',
+        type: 'dip'
+      },
+      {
+        name: 'translation',
+        repo: 'git://github.com/nachos/dip-translation.git',
+        type: 'dip'
+      },
+      function (err) {
+        if (err) {
+          deferred.reject(err);
+        } else {
+          deferred.resolve(_.toArray(arguments).slice(1));
+        }
+      });
   }, function (err) {
     if (err) {
       deferred.reject(err);
