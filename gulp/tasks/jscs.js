42921173fd012b03683a1b094be2728c13bf3d89
Noam Okman <noamokman@gmail.com>
2015-09-30 19:45:09 +0300

    updated dependencies and used ghooks

--- gulp/tasks/jscs.js
+++ gulp/tasks/jscs.js
@@ -8,16 +8,22 @@ module.exports = function (gulp) {
 
   gulp.task('jscs:server', function () {
     return gulp.src(config.paths.server)
-      .pipe(jscs());
+      .pipe(jscs())
+      .pipe(jscs.reporter())
+      .pipe(jscs.reporter('fail'));
   });
 
   gulp.task('jscs:test', function () {
     return gulp.src(config.paths.test)
-      .pipe(jscs());
+      .pipe(jscs())
+      .pipe(jscs.reporter())
+      .pipe(jscs.reporter('fail'));
   });
 
   gulp.task('jscs:gulp', function () {
     return gulp.src(config.paths.gulp)
-      .pipe(jscs());
+      .pipe(jscs())
+      .pipe(jscs.reporter())
+      .pipe(jscs.reporter('fail'));
   });
 };
